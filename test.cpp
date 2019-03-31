a0725675cc3a6b144cb2b5020c08ce23ae024a9d
Shivaprasad84 <shivaprasadsmys@gmail.com>
2019-03-31 08:46:02 +0530

    Testing

--- test.cpp
+++ test.cpp
@@ -8,6 +8,26 @@ int main()
 {
     Crypto c;
 
+    // string plaintext = "Attack at Dawn";
+    // c.write_to_file("plaintxt.txt", plaintext);
+
+    //*****************************************ENCRYPTION*************************************************//
+
+    // string read_plaintxt = c.read_from_file("plaintxt.txt");
+    // int c_key = 10;
+    // string caesar_enc = c.caesar_encrypt(read_plaintxt, c_key);
+    // c.write_to_file("caesar_enc.txt", caesar_enc);
+
+    // string read_caesar_enc = c.read_from_file("caesar_enc.txt");
+    // string vigenere_key = "Gold";
+    // string vigenere_enc = c.vigenere_encrypt(read_caesar_enc, vigenere_key);
+    // c.write_to_file("vigenere_enc.txt", vigenere_enc);
+
+
+    // Public Key: 34621
+    // Private Key: -25719
+    // n: 59989
+
     // int privkey, pubkey, n;
     // privkey = pubkey = n = 0;
     // c.genkeys_and_n(pubkey, privkey, n);
@@ -15,30 +35,22 @@ int main()
     // cout << "Private Key: " << privkey << endl;
     // cout << "n: " << n << endl;
 
+    // string read_vigenere_enc = c.read_from_file("vigenere_enc.txt");
+    // vector<unsigned int> rsa_enc = c.rsa_encrypt(read_vigenere_enc, 34621, 59989);
+    // c.write_rsa_encryption_file("rsa_enc.txt", rsa_enc);
 
-    // string msg_to_enc = c.read_from_file("plaintxt.txt");
-    // string caes_enc = c.caesar_encrypt(msg_to_enc, 6);
+    //****************************END OF ENCRYPTION****************************************//
 
-    // vector<unsigned int> vec = c.rsa_encrypt(caes_enc, 26251, 59989);
-    // c.write_rsa_encryption_file("cryptfile.txt", vec);
+    //*******************************DECRYPTION**********************************************//
 
-    // vector<unsigned int> v = c.read_rsa_encryption_file("cryptfile.txt");
-    // string dec_msg = c.rsa_decrypt(v, -8749, 59989);
-    // c.write_to_file("decrypt.txt", dec_msg);
+    vector<unsigned int> read_rsa_enc = c.read_rsa_encryption_file("rsa_enc.txt");
+    string rsa_dec = c.rsa_decrypt(read_rsa_enc, -25719, 59989);
+    cout << rsa_dec << endl;
+    string vigenere_dec = c.vigenere_decrypt(rsa_dec, "Gold");
+    cout << vigenere_dec << endl;
+    string caesar_dec = c.caesar_decrypt(vigenere_dec, 10);
+    cout << caesar_dec << endl;
+    c.write_to_file("decrypt.txt", caesar_dec);
 
-    // string x = c.read_from_file("decrypt.txt");
-    // string y = c.caesar_decrypt(x, 6);
-
-    // c.write_to_file("decrypt1.txt", y);
-
-    string msg, key;
-    cout << "Enter a message: ";
-    getline(cin, msg);
-    cout << "Enter key: ";
-    cin >> key;
-    string enc_msg = c.vigenere_encrypt(msg, key);
-    string dec_msg = c.vigenere_decrypt(enc_msg, key);
-
-    cout << "Encrypted: " << enc_msg << endl;
-    cout << "Decrypted: " << dec_msg << endl;
+    //****************************END OF DECRYPTION*******************************************************//
 }
